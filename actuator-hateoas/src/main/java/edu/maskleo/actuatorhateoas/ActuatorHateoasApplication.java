5f7330020473c469e560d0390f7ec98a2dfeaceb
maskleo <masque@aliyun.com>
2018-06-25 14:29:28 +0800

    init

---
+++ actuator-hateoas/src/main/java/edu/maskleo/actuatorhateoas/ActuatorHateoasApplication.java
@@ -0,0 +1,12 @@
+package edu.maskleo.actuatorhateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorHateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorHateoasApplication.class, args);
+    }
+}
